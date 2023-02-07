40e6a008745f702e415916be87ab65937a5a46dc
danielbdecker <danielbdecker@gmail.com>
2023-02-06 17:57:10 -0700

    Add project files.

---
+++ Mission05_dbdecker/Models/GradesModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission05_dbdecker.Models
+{
+    public class GradesModel
+    {
+        [Required]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
+        public int Assignments { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
+        public int GroupProject { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
+        public int Quizzes { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
+        public int MidtermExam { get; set; }
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
+        public int FinalExam { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
+        public int Intex { get; set; }
+
+    }
+}
---
+++ Mission05_dbdecker/wwwroot/js/grade.js
@@ -0,0 +1,46 @@
+﻿$("#btnSend").click(function () {
+    //grabs the values from the form and weights them together to get the grade for the class
+    var inputs = ['assignments', 'groupProject', 'quizzes', 'midtermExam', 'finalExam', 'intex'];
+
+    for (var i = 0; i < inputs.length; i++) {
+        var inputValue = $("#" + inputs[i]).val();
+        console.log(inputValue);
+        if (inputValue < 0 || inputValue > 100) {
+            return;
+            break;
+           
+        }
+    }
+    var grade = (($("#assignments").val() * 0.5) + ($("#groupProject").val() * 0.1) +
+        ($("#quizzes").val()*0.1) + ($("#midtermExam").val()*0.1) +
+        ($("#finalExam").val() * 0.1) + ($("#intex").val()) * 0.1);
+    var letterGrade
+    //checks what the final letter grade is
+    if (grade >= 94) {
+        letterGrade = "A";
+    } else if (grade >= 90) {
+        letterGrade = "A-";
+    } else if (grade >= 87) {
+        letterGrade = "B+";
+    } else if (grade >= 84) {
+        letterGrade = "B";
+    } else if (grade >= 80) {
+        letterGrade = "B";
+    } else if (grade >= 77) {
+        letterGrade = "C+";
+    } else if (grade >= 74) {
+        letterGrade = "C";
+    } else if (grade >= 70) {
+        letterGrade = "C-";
+    } else if (grade >= 67) {
+        letterGrade = "D+";
+    } else if (grade >= 64) {
+        letterGrade = "D";
+    } else if (grade >= 60) {
+        letterGrade = "D-";
+    } else {
+        letterGrade = "E";
+    }
+    //prints both values to a p tag in the form
+    alert(`The percentage was ${Math.round(grade)}, which is a(n) ${letterGrade}.`);
+});
